85682a69f45e2cc925c9d23c7cd637fd54ccacac
zishan344 <zishanahmed344@gmail.com>
2022-02-24 18:35:25 +0600

    done the user 6 number problem

---
+++ js/problem-6.js
@@ -0,0 +1,30 @@
+const randomUser = () => {
+  fetch("https://randomuser.me/api")
+    .then((response) => response.json())
+    .then((data) => randomUserDisplay(data.results[0]));
+};
+const randomUserDisplay = (data) => {
+  console.log(data);
+  const randomUserProfile = document.getElementById("randomUserProfile");
+  randomUserProfile.textContent = "";
+  const div = document.createElement("div");
+  div.classList.add("card");
+  div.classList.add("cards");
+  div.classList.add("shadow-lg");
+  div.innerHTML = `
+  <img  src="${data.picture.large}" class="card-img-top w-50 mx-auto" alt="." />
+  <div class="card-body">
+    <h5 class="card-title"> Name: ${data.name.title} ${data.name.first} ${data.name.last}</h5>
+    <h5>gender: ${data.gender}</h5>
+     <div class=" ">
+     <h5>Email: ${data.email}</h5>
+     <h5>Phone: ${data.phone}</h5>
+     <h5>country: ${data.location.country}</h5>
+     </div>
+     <div class="d-flex justify-content-center">
+    <a href="#" class="btn btn-primary ">see full detils</a>
+    </div>
+  </div>
+   `;
+  randomUserProfile.appendChild(div);
+};
