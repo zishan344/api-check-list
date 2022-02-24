d5a8aef2f83b1b0b85dc373fe0e0781157b44528
zishan344 <zishanahmed344@gmail.com>
2022-02-24 08:30:59 +0600

    done the 2number problem

---
+++ js/problem-2.js
@@ -0,0 +1,17 @@
+const loadData = () => {
+  fetch("https://jsonplaceholder.typicode.com/comments")
+    .then((response) => response.json())
+    .then((data) => displayComments(data));
+};
+const displayComments = (data) => {
+  let count = 1;
+  const example = document.getElementById("example");
+  console.log(example);
+  data.forEach((comment) => {
+    const p = document.createElement("p");
+    // console.log(comment.body);
+    p.innerText = `${count++}|  ${comment.body} `;
+    console.log(p);
+    example.appendChild(p);
+  });
+};
